addfbadea6d881a407b65fe337ea6f1318de4dcc
Celi Vidal <grupocapgemini-services.ernesto.celi.vidal@hp.com>
2021-05-19 12:01:37 +0200

    modal candidates shows the candidates and gives options to accept or reject them

--- src/Components/ModalCandidates/modalCandidates.view.js
+++ src/Components/ModalCandidates/modalCandidates.view.js
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
+import { ReactComponent as HalfLogo } from '../../Images/Spinner.svg';
 import { getUserToken } from '../../Utils/Auth';
 import { fetchMeStuff } from '../../Utils/functions';
 import Modal from '../Modal';
@@ -6,6 +8,37 @@ import styles from './modalCandidates.module.css';
 
 const ModalCandidates = ({ offer, handleClose }) => {
   const [candidatesList, setCandidatesList] = useState();
+  const [trigger, setTrigger] = useState(false);
+
+  const handleAccept = (id) => {
+    const options = {
+      method: 'PUT',
+      headers: new Headers({
+        Accept: 'apllication/json',
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${getUserToken()}`,
+      }),
+      mode: 'cors',
+      body: JSON.stringify({ companyAccepted: true }),
+    };
+
+    fetchMeStuff(`http://localhost:3001/offerstatus/${id}`, options, () => setTrigger(!trigger));
+  };
+
+  const handleReject = (id) => {
+    const options = {
+      method: 'PUT',
+      headers: new Headers({
+        Accept: 'apllication/json',
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${getUserToken()}`,
+      }),
+      mode: 'cors',
+      body: JSON.stringify({ companyRejected: true }),
+    };
+
+    fetchMeStuff(`http://localhost:3001/offerstatus/${id}`, options, () => setTrigger(!trigger));
+  };
 
   const options = {
     method: 'POST',
@@ -20,15 +53,52 @@ const ModalCandidates = ({ offer, handleClose }) => {
 
   useEffect(() => {
     fetchMeStuff('http://localhost:3001/offerstatus/candidates', options, setCandidatesList);
-  }, []);
+  }, [trigger]);
   return (
     <Modal>
       <div className={styles.container}>
-        {candidatesList && candidatesList.map((candidate) => <p>{candidate.userId.name}</p>)}
-        {candidatesList && candidatesList.length === 0 && (
-          <p>No candiates have liked this offer yet</p>
-        )}
-        <button type="button" onClick={handleClose}>
+        <div className={styles.wrapperContainer}>
+          {candidatesList &&
+            candidatesList.map((candidate) => {
+              let wrapperStyle = `${styles.rowWrapper}`;
+              if (candidate.companyAccepted === true) {
+                wrapperStyle = `${wrapperStyle} ${styles.companyAccepted}`;
+              } else if (candidate.companyRejected === true) {
+                wrapperStyle = `${wrapperStyle} ${styles.companyRejected}`;
+              }
+              return (
+                <div className={wrapperStyle} onClick={() => console.log(candidate)}>
+                  <p>{candidate.userId.name}</p>
+                  <div className={styles.optionsDiv}>
+                    <FontAwesomeIcon className={`${styles.icon} ${styles.user}`} icon="user" />
+                    {!candidate.companyAccepted && !candidate.companyRejected && (
+                      <>
+                        <FontAwesomeIcon
+                          className={`${styles.icon} ${styles.accept}`}
+                          icon="check"
+                          onClick={() => handleAccept(candidate._id)}
+                        />
+                        <FontAwesomeIcon
+                          className={`${styles.icon} ${styles.reject}`}
+                          icon="times"
+                          onClick={() => handleReject(candidate._id)}
+                        />
+                      </>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+          {candidatesList && candidatesList.length === 0 && (
+            <div className={styles.noCandidatesDiv}>
+              <div className={styles.halfLogoDiv}>
+                <HalfLogo className={styles.halfLogo} />
+              </div>
+              <p className={styles.noCandidates}>No candiates have liked this offer yet</p>
+            </div>
+          )}
+        </div>
+        <button type="button" onClick={handleClose} className={styles.closeButton}>
           Close
         </button>
       </div>
